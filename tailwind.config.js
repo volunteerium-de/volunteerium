289ab13c31d2e3ac57b0042d459ef7d9805e5a89
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-09-11 11:09:58 +0200

    initial folder-file structure + add tailwind configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        white: "#FFFFFF",
+        "light-gray": "#F8F9FC",
+        "light-gray-2": "#F1F3F9",
+        "light-gray-3": "#E1R6EF",
+        "gray-1": "#C1C2C4",
+        "gray-2": "#9D9EA1",
+        "dark-gray-1": "#6C707A",
+        "dark-gray-2": "#4A505C",
+        "dark-gray-3": "#3A3C41",
+        black: "#000000",
+        "light-green": "#DCE6E0",
+        "primary-green": "#69957B",
+        "dark-green": "#4B6D59",
+        warning: "#B25E09",
+        danger: "#AC242F",
+      },
+    },
   },
   plugins: [],
 };
