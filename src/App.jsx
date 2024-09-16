e02173e73483bc5ea0c8d915b1970c7a5351c1e0
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-09-16 10:49:11 +0200

    [chore] ofc/ Update dependencies: react-toastify
    - Add a helper file called toastNotify.js to create reusable toastify messages based on types.
    - Add ToastContainer in App.jsx (theme attribute (light | dark) will be updated soon after implementing theme functionality).

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import AppRouter from './router/AppRouter'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
 import store, { persistor } from './app/store'
+import { ToastContainer } from 'react-toastify'
 
 function App() {
   return (
@@ -11,6 +12,11 @@ function App() {
           <AppRouter />
         </PersistGate>
       </Provider>
+      <ToastContainer
+        autoClose={3000}
+        closeOnClick
+        // theme="light" | "dark" from local-storage or hook
+      />
     </>
   )
 }
