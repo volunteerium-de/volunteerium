9cf6e02db2b1db6315172cdeade9833a6ca63483
Burak Karaman <karaman.buraak@gmail.com>
2024-09-18 10:08:53 +0200

    [style] bk/BE-007 - Update hero & searchbar components

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -1,8 +1,8 @@
-import SearchBar from "./SearchBar";
+import SearchBar from "./SearchBar"
 
 const Hero = () => {
   return (
-    <div className="relative poppins-regular">
+    <div className="relative font-poppins">
       <div className="relative w-full h-[225px] sm:h-[425px]">
         <img
           className=" w-full h-full object-cover"
@@ -10,17 +10,17 @@ const Hero = () => {
           alt="Homepage hero background image"
         />
       </div>
-      <div className="absolute inset-0 flex items-center justify-center">
-        <h2 className="relative text-white text-2xl sm:text-3xl transform -translate-y-16">
+      <div className="absolute inset-0 bottom-20 sm:bottom-28 flex items-center justify-center">
+        <h2 className="relative text-white text-2xl sm:text-4xl transform -translate-y-16">
           Become a part of something
-          <span className="absolute top-8 -right-5 bad-script-regular">
+          <span className="absolute font-bad-script top-6 sm:top-8 -right-4 sm:-right-5 bad-script-regular text-[30px] sm:text-[48px]">
             bigger
           </span>
         </h2>
       </div>
       <SearchBar />
     </div>
-  );
-};
+  )
+}
 
-export default Hero;
+export default Hero
--- src/components/Home/SearchBar.jsx
+++ src/components/Home/SearchBar.jsx
@@ -1,11 +1,11 @@
-import { CiSearch } from "react-icons/ci";
+import { FaSearch } from "react-icons/fa"
 
 const SearchBar = () => {
   const searchBarItems = [
     {
       id: "event",
       label: "Event",
-      placeholder: "Search for opportunities",
+      placeholder: "Search opportunities",
       type: "input",
     },
     {
@@ -17,7 +17,7 @@ const SearchBar = () => {
     {
       id: "category",
       label: "Category",
-      placeholder: "Choose Category",
+      // placeholder: 'Choose Category',
       type: "select",
       options: [
         { value: "", label: "Choose Category", disabled: true, selected: true },
@@ -26,10 +26,10 @@ const SearchBar = () => {
         { value: "health", label: "Health" },
       ],
     },
-  ];
+  ]
 
   return (
-    <div className="relative w-[370px] sm:w-[80vw] lg:w-[60vw] mx-auto py-2 px-1 sm:p-2 bottom-20 sm:bottom-40 rounded-lg border border-white border-opacity-40 bg-white flex items-center justify-between">
+    <div className="relative h-[46px] sm:h-[64px] w-[370px] sm:w-[90vw] md:w-[750px] lg:w-[820px] mx-auto py-2 px-3 sm:py-2 sm:px-6 bottom-28 sm:bottom-48 rounded-lg border border-white border-opacity-50 bg-white flex items-center justify-between dark:bg-black dark:text-white  ">
       {searchBarItems.map(({ id, label, placeholder, type, options }) => (
         <div
           key={id}
@@ -37,10 +37,7 @@ const SearchBar = () => {
             id !== "event" && "border-l border-opacity-30 border-gray-300 pl-3"
           } `}
         >
-          <label
-            htmlFor={id}
-            className=" text-[12px] sm:text-sm md:text-base font-bold mb-1"
-          >
+          <label htmlFor={id} className=" text-[12px] sm:text-[16px] font-semibold mb-1">
             {label}
           </label>
           {type === "input" ? (
@@ -48,13 +45,13 @@ const SearchBar = () => {
               id={id}
               type="text"
               placeholder={placeholder}
-              className="text-[9px] sm:text-sm md:text-base focus:outline-none w-full"
+              className="text-[9px] sm:text-[16px] focus:outline-none w-full"
             />
           ) : (
             <select
               id={id}
               name={id}
-              className="text-[9px] sm:text-[0.9rem] pr-2 rounded-md focus:outline-none opacity-60"
+              className="text-[9px] sm:text-[0.9rem] rounded-md focus:outline-none appearance-none"
             >
               {options.map((option) => (
                 <option key={option.value} value={option.value}>
@@ -66,10 +63,10 @@ const SearchBar = () => {
         </div>
       ))}
       <button className="bg-primary-green w-8 h-8 sm:w-10 sm:h-10 text-white rounded-full flex items-center justify-center ml-2 sm:ml-0">
-        <CiSearch className="text-xl" />
+        <FaSearch className="text-[13px] sm:text-[16px]" />
       </button>
     </div>
-  );
-};
+  )
+}
 
-export default SearchBar;
+export default SearchBar
