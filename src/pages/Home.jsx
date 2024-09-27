95e4922cbd90025a4eed480a635998f582892f24
Fatmaud <fatmaudemirci@gmail.com>
2024-09-27 16:36:57 +0200

    Refactoring Get to Know Us

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,7 @@
-import React from 'react'
-import GetToKnowUs from '../components/Home/GetToKnowUs'
+// import React from 'react'
 
 const Home = () => {
-  return (
-    <div>
-      <GetToKnowUs />
-    </div>
-  )
+  return <div>Home</div>
 }
+
 export default Home
