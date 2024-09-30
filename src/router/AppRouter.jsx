b8367ef2a3fb44e64ed0b243e50f84d090842be7
Burak Karaman <karaman.buraak@gmail.com>
2024-09-30 13:45:44 +0200

    [feat] Add Profile page

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -5,18 +5,19 @@ import Register from "../pages/Register"
 import PrivateRouter from "./PrivateRouter"
 import NotFound from "../pages/NotFound"
 import AboutUs from "../pages/AboutUs"
-import EmailVerification from "../pages/EmailVerification";
+import Profile from "../pages/Profile"
+import EmailVerification from "../pages/EmailVerification"
 
 const AppRouter = () => {
   return (
     <Router>
-  <Routes>
+      <Routes>
         <Route path="/" element={<Home />} />
         <Route path="login" element={<Login />} />
         <Route path="register" element={<Register />} />
-	      <Route path="register/email-verify" element={<EmailVerification/>} />
+        <Route path="register/email-verify" element={<EmailVerification />} />
         <Route path="about" element={<AboutUs />} />
-
+        <Route path="profile" element={<Profile />} />
         <Route path="" element={<PrivateRouter />}>
           {/* <Route path="profile/:userId" element={<Profile />} /> */}
           {/* <Route path="" element={< />} /> */}
