2423988fa3a85b15ffcfeef4694b31dbb47a9229
sabanferik <sbnftmaf@gmail.com>
2024-10-01 13:44:51 +0200

    [feat] sf/FE-030 - add FAQuestion.jsx file

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -6,6 +6,7 @@ import PrivateRouter from "./PrivateRouter"
 import NotFound from "../pages/NotFound"
 import AboutUs from "../pages/AboutUs"
 import EmailVerification from "../pages/EmailVerification";
+import FAQuestion from "../pages/FAQuestion"
 
 const AppRouter = () => {
   return (
@@ -16,6 +17,7 @@ const AppRouter = () => {
         <Route path="register" element={<Register />} />
 	      <Route path="register/email-verify" element={<EmailVerification/>} />
         <Route path="about" element={<AboutUs />} />
+        <Route path="faq" element={<FAQuestion />} />
 
         <Route path="" element={<PrivateRouter />}>
           {/* <Route path="profile/:userId" element={<Profile />} /> */}
