a49aa6758bfc588d6c1beb8fdd9f57c51194c755
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-10-17 01:15:19 +0200

    [feat] refactor SocketContext to fetch and manage conversations using axiosWithToken

--- src/context/SocketContext.jsx
+++ src/context/SocketContext.jsx
@@ -3,6 +3,8 @@
 import React, { createContext, useContext, useEffect, useState } from "react"
 import { useSelector } from "react-redux"
 import io from "socket.io-client"
+import useAxios from "../hooks/useAxios"
+import toastNotify from "../utils/toastNotify"
 
 const SocketContext = createContext(null)
 
@@ -13,6 +15,12 @@ export const useSocket = () => {
 export const SocketProvider = ({ children }) => {
   const [socket, setSocket] = useState(null)
   const { bearer } = useSelector((state) => state.auth)
+  const [conversations, setConversations] = useState([])
+  const { axiosWithToken } = useAxios()
+
+  useEffect(() => {
+    fetchConversations()
+  }, [])
 
   useEffect(() => {
     if (bearer) {
@@ -27,6 +35,53 @@ export const SocketProvider = ({ children }) => {
       }
     }
   }, [bearer])
+
+  useEffect(() => {
+    if (socket) {
+      socket.on("receive_conversations", () => {
+        fetchConversations()
+      })
+
+      return () => {
+        socket.off("receive_conversations")
+      }
+    }
+  }, [socket])
+
+  const fetchConversations = async () => {
+    const { data } = await axiosWithToken.get(`/conversations`)
+    console.log(data)
+    setConversations(data.data)
+  }
+
+  const createConversation = async (eventId, participantId) => {
+    if ((eventId, participantId)) {
+      try {
+        await axiosWithToken.post(`/conversations`, {
+          eventId,
+          participantIds: [participantId],
+        })
+      } catch (error) {
+        // console.log(error);
+        toastNotify("error", error.message)
+      }
+    } else {
+      toastNotify("error", "Event ID or Participant ID are missing.")
+    }
+  }
+
+  const deleteConversation = async (conversationId) => {
+    if (conversationId) {
+      try {
+        await axiosWithToken.delete(`/conversations/${conversationId}`)
+      } catch (error) {
+        // console.log(error)
+        toastNotify("error", error.message)
+      }
+    } else {
+      toastNotify("error", "Conversation ID is missing.")
+    }
+  }
 
   return <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>
 }
