ec1dede15c136d2a52628d3c21fad3836faa787f
tarti <tartilaci@gmail.com>
2024-10-24 15:56:12 +0200

    [feat] akt/FE-039 - Add internationalization support to components of UserSetting page, including translations for German and English languages.
    (refactor) - Add internationalization support to Yup messages in LoginForm and RegisterForm

--- src/locales/translations.js
+++ src/locales/translations.js
@@ -1,179 +1,307 @@
-import Subscribe from "../components/Footer/Subscribe";
+import Subscribe from "../components/Footer/Subscribe"
 
 export const translations = {
-    heroSection: {
-      h2: "hero.h2",
-      span: "hero.span"
+  heroSection: {
+    h2: "hero.h2",
+    span: "hero.span",
+  },
+
+  searchbar: {
+    event: {
+      label: "searchbar.event.label",
+      placeholder: "searchbar.event.placeholder",
     },
-
-    searchbar: {
-        event: {
-            label: "searchbar.event.label",
-            placeholder: "searchbar.event.placeholder"},
-        location: {
-            label: "searchbar.location.label",
-            placeholder: "searchbar.location.placeholder"
-        },
-        category: {
-            label: "searchbar.category.label",
-            placeholder: "searchbar.category.placeholder"
-        }
+    location: {
+      label: "searchbar.location.label",
+      placeholder: "searchbar.location.placeholder",
     },
-
-    upcomingOpp: {
-        title: "upcomingOpportunities.title",
-        discover: "upcomingOpportunities.discoverMore"
+    category: {
+      label: "searchbar.category.label",
+      placeholder: "searchbar.category.placeholder",
     },
+  },
 
-    getToKnowUs: {  
-        h2: "getToKnowUs.h2", 
-        description1: "getToKnowUs.description1", 
-        description2: "getToKnowUs.description2", 
-        description3: "getToKnowUs.description3", 
-        imageAlt: "getToKnowUs.imageAlt"
+  upcomingOpp: {
+    title: "upcomingOpportunities.title",
+    discover: "upcomingOpportunities.discoverMore",
+  },
+
+  getToKnowUs: {
+    h2: "getToKnowUs.h2",
+    description1: "getToKnowUs.description1",
+    description2: "getToKnowUs.description2",
+    description3: "getToKnowUs.description3",
+    imageAlt: "getToKnowUs.imageAlt",
+  },
+
+  subscribe: {
+    h3: "subscribe.h3",
+    p: "subscribe.p",
+    emailPH: "subscribe.emailPH",
+  },
+
+  footer: {
+    // Yeni ekleme
+    information: "footer.information",
+    company: "footer.company",
+    aboutUs: "footer.aboutUs",
+    careers: "footer.careers",
+    investors: "footer.investors",
+    h2: "footer.h2",
+    faq: "footer.faq",
+    contact: "footer.contact",
+    privacy: "footer.privacy",
+    terms: "footer.terms",
+    p: "footer.p",
+  },
+
+  register: {
+    h1: "register.h1",
+    accountQuestion: "register.accountQuestion",
+    p1: "register.p1",
+    p2: "register.p2",
+    p3: "register.p3",
+    p4: "register.p4",
+    p5: "register.p5",
+    p6: "register.p6",
+    p7: "register.p7",
+    createAccount: "register.createAccount",
+    creatingAs: "register.creatingAs",
+  },
+
+  registerForm: {
+    yup1: "registerForm.yup1",
+    yup2: "registerForm.yup2",
+    yup3: "registerForm.yup3",
+    yup4: "registerForm.yup4",
+    yup5: "registerForm.yup5",
+    yup6: "registerForm.yup6",
+    yup7: "registerForm.yup7",
+    yup8: "registerForm.yup8",
+    yup9: "registerForm.yup9",
+    yup10: "registerForm.yup10",
+    yup11: "registerForm.yup11",
+    yup12: "registerForm.yup12",
+    yup13: "registerForm.yup13",
+    radioIndv: "registerForm.radioIndv",
+    radioOrg: "registerForm.radioOrg",
+    fullname: "registerForm.fullname",
+    orgname: "registerForm.orgname",
+    fullnamePH: "registerForm.fullnamePH",
+    orgnamePH: "registerForm.orgnamePH",
+    email: "registerForm.email",
+    emailPH: "registerForm.emailPH",
+    password: "registerForm.password",
+    passwordPH: "registerForm.passwordPH",
+    submit: "registerForm.submit",
+    haveAccount: "registerForm.haveAccount",
+    login: "registerForm.login",
+    or: "registerForm.or",
+    contGoogle: "registerForm.contGoogle",
+    pTerms: "registerForm.pTerms",
+    terms: "registerForm.terms",
+    pPrivacy: "registerForm.pPrivacy",
+    privacy: "registerForm.privacy",
+  },
+
+  login: {
+    p1: "login.p1",
+    p2: "login.p2",
+    p3: "login.p3",
+    p4: "login.p4",
+    p5: "login.p5",
+    p6: "login.p6",
+    p7: "login.p7",
+    signIn: "login.signIn",
+  },
+
+  loginForm: {
+    yup1: "loginForm.yup1",
+    yup2: "loginForm.yup2",
+    yup3: "loginForm.yup3",
+    yup4: "loginForm.yup4",
+    yup5: "loginForm.yup5",
+    yup6: "loginForm.yup6",
+    yup7: "loginForm.yup7",
+    yup8: "loginForm.yup8",
+    yup9: "loginForm.yup9",
+    email: "loginForm.email",
+    emailPH: "loginForm.emailPH",
+    password: "loginForm.password",
+    passwordPH: "loginForm.passwordPH",
+    forgot: "loginForm.forgot",
+    login: "loginForm.login",
+    haveAccount: "loginForm.haveAccount",
+    signUp: "loginForm.signUp",
+    or: "loginForm.or",
+    contGoogle: "loginForm.contGoogle",
+  },
+
+  profile: {
+    memberSince: "profile.memberSince",
+    edit: "profile.edit",
+    attendedEvents: "profile.attendedEvents",
+    createdEvents: "profile.createdEvents",
+    loading: "profile.loading",
+    interests: "profile.interests",
+    aboutMe: "profile.aboutMe",
+    documents: "profile.documents",
+    medals: {
+      goldenHeart: "profile.medals.goldenHeart",
+      silverMedal: "profile.medals.silverMedal",
+      bronzeMedal: "profile.medals.bronzeMedal",
+      newVolunteer: "profile.medals.newVolunteer",
+      goldenInfo: "profile.medals.goldenInfo",
+      silverInfo: "profile.medals.silverInfo",
+      bronzeInfo: "profile.medals.bronzeInfo",
     },
+    avatarAlt: "profile.avatarAlt",
+    logoAlt: "profile.logoAlt",
+    eventImageAlt: "profile.eventImgAlt",
+  },
 
-    subscribe: {
-        h3: "subscribe.h3",
-        p: "subscribe.p",
-        emailPH: "subscribe.emailPH"
-    },
+  userSettings: {
+    avatarAlt: "userSettings.avatarAlt",
+    edit: "userSettings.edit",
+    welcome: "userSettings.welcome",
+    profile: "userSettings.profile",
+    security: "userSettings.security",
+    visibility: "userSettings.visibility",
+  },
 
-    footer: { // Yeni ekleme
-        information: "footer.information",
-        company: "footer.company",
-        aboutUs: "footer.aboutUs",
-        careers: "footer.careers",
-        investors: "footer.investors",
-        h2: "footer.h2",
-        faq: "footer.faq",
-        contact: "footer.contact",
-        privacy: "footer.privacy",
-        terms: "footer.terms",
-        p: "footer.p"
-    },
+  addNewDoc: {
+    newDoc: "addNewDoc.newDocument",
+    docTitle: "addNewDoc.documentTitle",
+    reset: "addNewDoc.buttonReset",
+    addNew: "addNewDoc.buttonAddNew",
+    cancel: "addNewDoc.cancel",
+    save: "addNewDoc.save",
+  },
 
-    register: {
-        h1: "register.h1",
-        accountQuestion: "register.accountQuestion",
-        p1: "register.p1",
-        p2: "register.p2",
-        p3: "register.p3",
-        p4: "register.p4",
-        p5: "register.p5",
-        p6: "register.p6",
-        p7: "register.p7",
-        createAccount: "register.createAccount",
-        creatingAs: "register.creatingAs"
-      },
+  avatarEdit: {
+    networkError: "avatarEdit.networkError",
+    photoAlert: "avatarEdit.photoAlert",
+    photoError: "avatarEdit.photoError",
+    loading: "avatarEdit.loading",
+    logo: "avatarEdit.logo",
+    profilePhoto: "avatarEdit.profilePhoto",
+    avatarAlt: "avatarEdit.avatarAlt",
+    addPhoto: "avatarEdit.addPhoto",
+    delete: "avatarEdit.delete",
+  },
 
-    registerForm: {
-        radioIndv: "registerForm.radioIndv",
-        radioOrg: "registerForm.radioOrg",
-        fullname: "registerForm.fullname",
-        orgname: "registerForm.orgname",
-        fullnamePH: "registerForm.fullnamePH",
-        orgnamePH: "registerForm.orgnamePH",
-        email: "registerForm.email",
-        emailPH: "registerForm.emailPH",
-        password: "registerForm.password",
-        passwordPH: "registerForm.passwordPH",
-        submit: "registerForm.submit",
-        haveAccount: "registerForm.haveAccount",
-        login: "registerForm.login",
-        or: "registerForm.or",
-        contGoogle: "registerForm.contGoogle",
-        pTerms: "registerForm.pTerms",
-        terms: "registerForm.terms",
-        pPrivacy: "registerForm.pPrivacy",
-        privacy: "registerForm.privacy"
-    },
+  delAccount: {
+    h2: "delAccount.h2",
+    p: "delAccount.p",
+    cancelButton: "delAccount.cancelButton",
+    delButton: "delAccount.delButton",
+  },
 
-    login: {
-        p1 : "login.p1",
-        p2 : "login.p2",
-        p3 : "login.p3",
-        p4 : "login.p4",
-        p5 : "login.p5",
-        p6 : "login.p6",
-        p7 : "login.p7",
-        signIn : "login.signIn"
-      },
-    
-    loginForm: {
-        email : "loginForm.email",
-        emailPH: "loginForm.emailPH",
-        password : "loginForm.password",
-        passwordPH : "loginForm.passwordPH",
-        forgot: "loginForm.forgot",
-        login: "loginForm.login",
-        haveAccount: "loginForm.haveAccount",
-        signUp: "loginForm.signUp",
-        or: "loginForm.or",
-        contGoogle: "loginForm.contGoogle"
-    },
+  indvSettings: {
+    success: "indvSettings.success",
+    catchError: "indvSettings.catchError",
+    elseError: "indvSettings.elseError",
+    h1: "indvSettings.h1",
+    p1: "indvSettings.p",
+    p2: "indvSettings.p2",
+    label1: "indvSettings.label1",
+    label1PH: "indvSettings.label1PH",
+    label2: "indvSettings.label2",
+    label2PH: "indvSettings.label2PH",
+    label3: "indvSettings.label3",
+    label4: "indvSettings.label4",
+    label5: "indvSettings.label5",
+    label6: "indvSettings.label6",
+    label6PH: "indvSettings.label6PH",
+    label7: "indvSettings.label7",
+    edit: "indvSettings.edit",
+    save: "indvSettings.save",
+  },
 
-    profile: {
-        memberSince: "profile.memberSince",
-        edit: "profile.edit",
-        attendedEvents: "profile.attendedEvents",
-        createdEvents: "profile.createdEvents",
-        loading: "profile.loading",
-        interests : "profile.interests",
-        aboutMe : "profile.aboutMe",
-        documents: "profile.documents",
-        medals: {
-          goldenHeart: "profile.medals.goldenHeart",
-          silverMedal: "profile.medals.silverMedal",
-          bronzeMedal: "profile.medals.bronzeMedal",
-          newVolunteer: "profile.medals.newVolunteer",
-          goldenInfo: "profile.medals.goldenInfo",
-          silverInfo: "profile.medals.silverInfo",
-          bronzeInfo: "profile.medals.bronzeInfo"
-        },
-        avatarAlt: "profile.avatarAlt",
-        logoAlt: "profile.logoAlt",
-        eventImageAlt: "profile.eventImgAlt"
-    },
+  myDocs: {
+  alertSuccess: "myDocs.alertSuccess",
+  alertError: "myDocs.alertError",
+  h2: "myDocs.h2",
+  p: "myDocs.p",
+  addNew: "myDocs.addNew",
+  delete: "myDocs.delete",
+  update: "myDocs.update",
+  cancel: "myDocs.cancel",
+  saveChanges: "myDocs.saveChanges"
+},
 
-    userSettings: {
-        avatarAlt : "userSettings.avatarAlt",
-        edit : "userSettings.edit",
-        welcome: "userSettings.welcome",
-        profile: "userSettings.profile",
-        security: "userSettings.security",
-        visibility: "userSettings.visibility"
-    },
+orgSettings: {
+  handleErrorMsg: "orgSettings.handleErrorMsg",
+  successMsg: "orgSettings.successMsg",
+  errorMsg: "orgSettings.errorMsg",
+  h1: "orgSettings.h1",
+  label1: "orgSettings.label1",
+  label1PH: "orgSettings.label1PH",
+  label2: "orgSettings.label2",
+  label2PH: "orgSettings.label2PH",
+  label3: "orgSettings.label3",
+  label3PH: "orgSettings.label3PH",
+  label4: "orgSettings.label4",
+  label4PH: "orgSettings.label4PH",
+  label5: "orgSettings.label5",
+  label5PH: "orgSettings.label5PH",
+  label6: "orgSettings.label6",
+  label6PH: "orgSettings.label6PH",
+  label7: "orgSettings.label7",
+  label7PH: "orgSettings.label7PH",
+  label8: "orgSettings.label8",
+  label8PH: "orgSettings.label8PH",
+  files: "orgSettings.files",
+  edit: "orgSettings.edit",
+  save: "orgSettings.save"
+},
 
-    addNewDoc: {
-        newDoc: "addNewDoc.newDocument",
-        docTitle: "addNewDoc.documentTitle",
-        reset: "addNewDoc.buttonReset",
-        addNew: "addNewDoc.buttonAddNew",
-        cancel: "addNewDoc.cancel",
-        save: "addNewDoc.save"
-    },
+pswModal: {
+  yup1: "pswModal.yup1",
+  yup2: "pswModal.yup2",
+  yup3: "pswModal.yup3",
+  yup4: "pswModal.yup4",
+  yup5: "pswModal.yup5",
+  changePsw: "pswModal.changePsw",
+  currPsw: "pswModal.currPsw",
+  newPsw: "pswModal.newPsw",
+  confirmPsw: "pswModal.confirmPsw",
+  cancel: "pswModal.cancel",
+  saveChng: "pswModal.saveChng"
+},
 
-    avatarEdit: {
-        networkError: "avatarEdit.networkError",
-        photoAlert: "avatarEdit.photoAlert",
-        photoError: "avatarEdit.photoError",
-        loading: "avatarEdit.loading",
-        logo: "avatarEdit.logo",
-        profilePhoto: "avatarEdit.profilePhoto",
-        avatarAlt: "avatarEdit.avatarAlt",
-        addPhoto: "avatarEdit.addPhoto",
-        delete: "avatarEdit.delete"
-      },
+profileSettings: {
+    p: "profileSettings.p"
+},
 
-      delAccount: {
-        h2: "delAccount.h2",
-        p: "delAccount.p",
-        cancelButton: "delAccount.cancelButton",
-        delButton: "delAccount.delButton"
-      }
-      
+secSett: {
+    feedbackMsg1: "secSett.feedbackMsg1",
+    feedbackMsg2: "secSett.feedbackMsg2",
+    h1: "secSett.h1",
+    name: "secSett.name",
+    namePH: "secSett.namePH",
+    orgName: "secSett.orgName",
+    orgNamePH: "secSett.orgNamePH",
+    email: "secSett.email",
+    emailPH: "secSett.emailPH",
+    changePsw: "secSett.changePsw",
+    delAccount: "secSett.delAccount",
+    delAlert1: "secSett.delAlert1",
+    delAlert2: "secSett.delAlert2",
+    userRes1: "secSett.userRes1",
+    userRes2: "secSett.userRes2"
+},
+updateDoc: {
+    error: "updateDoc.error",
+    h2: "updateDoc.h2",
+    h1: "updateDoc.h1",
+    reset: "updateDoc.reset",
+    selectFile: "updateDoc.selectFile",
+    cancel: "updateDoc.cancel",
+    update: "updateDoc.update"
+}
 
 
-      
+
+
 }
