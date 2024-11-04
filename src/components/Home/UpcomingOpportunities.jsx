e3063dfe8bf13fe02f3c2ba92c001b160fcbcfaf
tarti <tartilaci@gmail.com>
2024-11-04 11:27:50 +0100

    [refactor] akt/FE-008 -Fixes to Upco.Opp and EventCardVert. components
    EventCardsVertical:
    Change Join button to More.
    -More button navigate to EventDetails
    -Go to Profile page when clicking on avatar or name
    -Add default event photo
    
    UpcommingOpportinuties:
    -Add Loading state
    -Add Loading and No Upc.Events texts

--- src/components/Home/UpcomingOpportunities.jsx
+++ src/components/Home/UpcomingOpportunities.jsx
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom"
 import { useTranslation } from "react-i18next"
 import { translations } from "../../locales/translations"
 import useEventCall from "../../hooks/useEventCall"
+import { ImSpinner9 } from "react-icons/im"
 
 const UpcomingOpportunities = () => {
   const { t } = useTranslation()
@@ -13,6 +14,7 @@ const UpcomingOpportunities = () => {
   const [isLeftDisabled, setIsLeftDisabled] = useState(true)
   const [isRightDisabled, setIsRightDisabled] = useState(false)
   const [eventData, setEventData] = useState([])
+  const [loading, setLoading] = useState(true)
 
   const { getEvents } = useEventCall()
 
@@ -57,22 +59,26 @@ const UpcomingOpportunities = () => {
     checkButtonStates()
   }, [scrollPosition, eventData.length])
 
-
   const fetchEvents = async () => {
+    setLoading(true)
     try {
-      const response = await getEvents("events/?filter[isActive]=true&sort[startDate]=desc");
-      console.log("Fetched response:", response);
-      setEventData(response.data)  
+      const response = await getEvents(
+        "events/?filter[isActive]=true&filter[isDone]=false&sort[startDate]=asc"
+      )
+      console.log("Fetched response:", response)
+      setEventData(response.data)
     } catch (error) {
-      console.error("Error fetching events:", error);
+      console.error("Error fetching events:", error)
+    } finally {
+      setLoading(false)
     }
-  };
+  }
 
- useEffect(() => {
-  fetchEvents()
-}, [])
+  useEffect(() => {
+    fetchEvents()
+  }, [])
 
-console.log(eventData)
+  console.log(eventData)
 
   return (
     <div className="pt-5">
@@ -116,12 +122,15 @@ console.log(eventData)
           >
             {/* Map through event data */}
             <div className="flex flex-row flex-grow gap-6 p-2 min-h-[200px] rounded-md">
-            {eventData.length > 0 ? (
-                eventData.map((event, _id) => (
-                  <EventCardVertical key={event._id} event={event} />
-                ))
+              {loading ? (
+                <div className="flex items-center gap-2">
+                  <ImSpinner9 className="animate-spin text-primary-green" />
+                  <span>Loading Upcomming Events...</span>
+                </div>
+              ) : eventData.length > 0 ? (
+                eventData.map((event, _id) => <EventCardVertical key={event._id} event={event} />)
               ) : (
-                <p>Loading events...</p>
+                <p>No Upcomming Events...</p>
               )}
             </div>
           </div>
--- src/components/ui/Cards/EventCardVertical.jsx
+++ src/components/ui/Cards/EventCardVertical.jsx
@@ -1,54 +1,73 @@
 import { IoCalendar, IoHome, IoLocation, IoPeople } from "react-icons/io5"
-import eventImage from "../../../assets/example-event-img.png"
+import defaulEventPhoto from "../../../assets/default-event-photo-.jpg"
 import { useSelector } from "react-redux"
-import { useEffect, useState } from "react"
 import { MdLanguage } from "react-icons/md"
-import { getLangName } from "./EventCardHorizontal"
-import useEventCall from "../../../hooks/useEventCall"
+import useLanguageOptions from "../../../hooks/useLanguages"
+import { formatName } from "../../../helpers/formatName"
+import { formatDate } from "../../../helpers/formatDate"
+import { UserAvatar } from "../Avatar/userAvatar"
+import { Link, useNavigate } from "react-router-dom"
 
-
-const EventCardVertical = ({event}) => {
+const EventCardVertical = ({ event }) => {
   console.log("event in EventCard", event)
   const { currentUser: user } = useSelector((state) => state.auth)
+  const navigate = useNavigate()
+  const { getLangName } = useLanguageOptions()
 
   const startDate = new Date(event.startDate).toLocaleDateString()
 
   return (
-      <div
+    <div
       key={event._id}
       className="flex flex-col justify-between rounded max-w-[200px] sm:max-w-[330px] shadow-[0_1px_1px_rgba(0,0,0,.25)] overflow-x-hidden shrink-0 mb-2 dark:bg-dark-gray-3"
     >
       {/* Event Image */}
       <div>
         <img
-          src={event.eventPhoto || eventImage}
+          src={event?.eventPhoto || defaulEventPhoto}
           alt="event"
           className="rounded-t-sm object-cover w-full h-[200px]"
         />
       </div>
       {/* Event Content */}
-      <div className="p-2 mb-2 h-full">
-        <h2 className="text-dark-gray-1 dark:text-white font-bold text-[0.9375rem] ">
+      <div className="p-2 h-full">
+        <h2 
+        className="text-dark-gray-1 dark:text-white font-bold text-[0.9375rem] ">
           {event.title}
         </h2>
-        <p className="text-dark-gray-1 dark:text-light-gray-2 text-[0.75rem] mb-[10px]">
+        <p className="text-dark-gray-1 dark:text-light-gray-2 text-[0.75rem] mb-[10px] h-[3rem]"
+        >
           {event.description.split(" ").slice(0, 10).join(" ")}
           {event.description.split(" ").length > 10 ? "..." : ""}
         </p>
         {/* Details */}
+        
         <div>
           {/* User */}
-          <div className="flex gap-x-3 items-center mb-[7px] py-1">
-            <IoHome className="text-primary-green" />
-            <p className="text-gray-2 dark:text-light-gray-2 font-semibold text-sm">
-              {event.createdBy?.fullName}
+          <Link
+                to={`/profile/${event?.createdBy?._id}`}
+                className="flex items-center"
+              >
+          <div className="flex gap-x-3 items-center mb-[10px] ">
+            <UserAvatar user={event.createdBy} size="h-6 w-6" backgroundActive={true} />
+            <p className="text-primary-green">
+              {event.createdBy?.userType === "individual"
+                ? formatName(
+                    event.createdBy?.fullName,
+                    event.createdBy?.userDetailsId?.isFullNameDisplay
+                  )
+                : event.createdBy?.organizationName}
             </p>
           </div>
+          </Link>
+
           {/* Event Details */}
           <div>
             <div className="flex gap-x-[8px] items-center mb-[1px]">
               <IoCalendar className="text-primary-green" />
-              <p className="text-gray-2 dark:text-light-gray-2 text-sm p-1">{startDate}</p>
+              <p className="text-gray-2 dark:text-light-gray-2 text-sm p-1">
+                {formatDate(startDate)}
+              </p>
             </div>
 
             {event.addressId && (
@@ -69,42 +88,47 @@ const EventCardVertical = ({event}) => {
               </p>
             </div>
 
-              {/* Event Languages */}
-              {event.languages.length > 0 && 
-                <div className="flex gap-x-[8px] items-center  mb-[10px]">
+            {/* Event Languages */}
+            {event.languages.length > 0 && (
+              <div className="flex gap-x-[8px] items-center  mb-[1px]">
                 <MdLanguage className="text-primary-green" />
                 <p className="text-gray-2 dark:text-light-gray-2 text-sm p-1">
                   {event.languages.map((langCode) => getLangName(langCode)).join(", ")}
                 </p>
-              </div>}
-              
-
-            {/* Category Area */}
-            <div className="flex flex-row flex-wrap gap-2  ">
-              {event.interestIds?.map((interest, index) => (
-                <div
-                  key={index}
-                  className="border border-primary-green dark:border-gray-1 px-2 py-1 rounded-full w-fit h-6"
-                >
-                  <p className="font-semibold tracking-wide text-[0.6rem] sm:text-[0.6rem] text-primary-green text-center dark:text-gray-1">
-                  {interest.name.toUpperCase()}
-                  </p>
-                </div>
-              ))}
-            </div>
+              </div>
+            )}
           </div>
         </div>
       </div>
-      {/* Event Button */}
-      <div className="flex flex-row p-2 justify-end">
-        {user ? (
-          <button className="font-medium text-white text-[0.9rem] text-center bg-primary-green px-3 py-1 rounded">
-            Join
-          </button>
-        ) : null}
+
+      <div>
+        {/* Category Area */}
+        <div className="flex flex-row flex-wrap gap-2  ">
+          {event.interestIds?.map((interest, index) => (
+            <div
+              key={index}
+              className="border border-primary-green dark:border-gray-1 px-2 py-1 rounded-full w-fit h-6"
+            >
+              <p className="font-semibold tracking-wide text-[0.6rem] sm:text-[0.6rem] text-primary-green text-center dark:text-gray-1">
+                {interest.name.toUpperCase()}
+              </p>
+            </div>
+          ))}
+        </div>
+        {/* Event Button */}
+        <div className="flex flex-row p-2 justify-end">
+          {user ? (
+            <button
+              onClick={() => navigate(`events/${event._id}`)}
+              className="font-medium text-white text-[0.9rem] text-center bg-primary-green px-3 py-1 rounded"
+            >
+              More
+            </button>
+          ) : null}
+        </div>
       </div>
     </div>
   )
-};
+}
 
 export default EventCardVertical
