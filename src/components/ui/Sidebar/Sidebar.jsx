4581a7219e66cbbd7628b3fd024542085f45cf24
Nida Kasap <nidakasapp@gmail.com>
2024-11-05 11:40:05 +0100

    Adjust minor size issue

--- src/components/ui/Sidebar/Sidebar.jsx
+++ src/components/ui/Sidebar/Sidebar.jsx
@@ -3,6 +3,7 @@ import { useSelector } from "react-redux"
 import { UserAvatar } from "../Avatar/userAvatar"
 import { useTranslation } from "react-i18next"
 import { translations } from "../../../locales/translations"
+import { Link } from "react-router-dom"
 
 const Sidebar = ({ items, activeTab, onTabChange, onEditAvatar }) => {
   const { currentUser: user } = useSelector((state) => state.auth)
@@ -14,8 +15,8 @@ const Sidebar = ({ items, activeTab, onTabChange, onEditAvatar }) => {
   }
 
   return (
-    <div className="h-screen max-h-[90vh]">
-      <div className="sm:w-[350px] w-[80px] sm:min-w-[300px] mt-3 sm:h-[95%] h-[70%] py-[30px] bg-light-gray dark:bg-dark-gray-3 transition-all duration-300 ease-in-out shadow-sm rounded-r-lg ">
+    <div className="h-screen max-h-[80vh] m-3 mr-3">
+      <div className="w-[85px] sm:w-[240px] lg:w-[300px] 2xl:w-[350px] h-[88vh] py-[30px] bg-light-gray dark:bg-dark-gray-3 rounded-lg">
         <div className="flex flex-col items-center gap-4">
           <div>
             <UserAvatar user={user} size="h-24 w-24 sm:h-32 sm:w-32 p-4" />
@@ -49,9 +50,8 @@ const Sidebar = ({ items, activeTab, onTabChange, onEditAvatar }) => {
                 onClick={() => onTabChange(item.key)}
               >
                 {item.icon}
-                <a href="#" className=" flex-1 sm:block hidden">
-                  {item.label}
-                </a>
+                <Link className=" flex-1 sm:block hidden">{item.label}</Link>
+
                 {activeTab === item.key && (
                   <span className="sm:w-[16px] w-[8px] sm:h-[50px] h-[50px] bg-dark-green dark:bg-primary-green absolute right-[-0px] top-0"></span>
                 )}
